9a8239232ec186b542188ad133add8ab67ad0ac2
VAAB <victor_15_avila@hotmail.es>
2025-12-18 00:20:47 -0300

    endpoint

--- src/lib/axiosClient.ts
+++ src/lib/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://amicito-001-site1.qtempurl.com/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://victoravila-001-site1.ktempurl.com/api",
   headers: 
   {
     "Content-Type": "application/json",
