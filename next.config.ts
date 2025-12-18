46b0446f0d027911efe42b803e93268f7dd5adce
VAAB <victor_15_avila@hotmail.es>
2025-12-17 23:36:20 -0300

    vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  output: "export",
-
-  basePath: isProd ? "/dashboard-time" : "",
-  assetPrefix: isProd ? "/dashboard-time/" : "",
-
-  trailingSlash: true,
-
   images: {
     unoptimized: true,
   },
